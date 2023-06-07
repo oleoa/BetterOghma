908c23bba990cac45ffc24f6f8dc7cbb4a84df08
LeoAbreu <99770307+AbreuDProgrammer@users.noreply.github.com>
2023-06-07 17:41:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Better Oghma
+// @name        BetterOghma
 // @namespace   Violentmonkey Scripts
 // @match       https://oghma.epcc.pt/*
 // @grant       none
@@ -7,6 +7,10 @@
 // @author      -
 // @description 07/06/2023, 11:03:25
 // ==/UserScript==
+
+// -------------------------------------------------- Configurações --------------------------------------------------
+const AVARAGE_DECIMAL_PARTS = 2;
+const DECREASE = true;
 
 var defaultCallBack = function(element){}
 var hide = function(element)
@@ -16,6 +20,10 @@ var hide = function(element)
 var grid = function(element)
 {
   element.style.display = "grid";
+}
+var flex = function(element)
+{
+  element.style.display = "flex";
 }
 
 function get(pattern, callback = defaultCallBack)
@@ -39,7 +47,7 @@ function getAll(pattern, callback = defaultCallBack)
     callback(element[i]);
 }
 
-function getByClass(className, callback = defaultCallBack)
+async function getByClass(className, callback = defaultCallBack)
 {
   let element = document.getElementsByClassName(className);
 
@@ -47,7 +55,7 @@ function getByClass(className, callback = defaultCallBack)
     return;
 
   for(var i = 0; i < element.length; i++)
-    callback(element[i]);
+    await callback(element[i]);
 }
 
 function getByText(text, callback = defaultCallBack)
@@ -59,9 +67,24 @@ function getByText(text, callback = defaultCallBack)
       callback(elements[i]);
 }
 
+function getAvarage(page)
+{
+  var avarage = 0;
+  var sumAll = 0;
+  var countAll = 0;
+  page.querySelectorAll("tr").forEach(function(tr){
+    if(tr.className != "header"){
+      var tds = tr.querySelectorAll("td");
+      sumAll += parseInt(tds[2].textContent);
+      countAll++;
+    }
+  });
+  avarage = sumAll / countAll;
+  return avarage;
+}
+
 var url = window.location.href;
 var page = url.split("/")[url.split("/").length-1];
-console.log(page);
 switch(page)
 {
 
@@ -80,7 +103,7 @@ switch(page)
           sons[i].style.display = "flex";
           if(i == 2){
             sons[i].style.justifyContent = "end";
-            element.style.order = '-'+sons[i].textContent;
+            element.style.order = (DECREASE?'-':'')+sons[i].textContent;
             if(element.className == "header")
               element.style.order = '-21';
           }
@@ -92,10 +115,52 @@ switch(page)
 
     });
 
+    getByClass("well clearfix", function(element){
+      var avarage = getAvarage(document);
+      var averageElement = document.createElement("p");
+      averageElement.textContent = "O aluno tem uma média de "+avarage.toFixed(AVARAGE_DECIMAL_PARTS)+" pontos";
+      element.appendChild(averageElement);
+    });
+
+    break;
+
+  // -------------------------------------------------- Pessoas --------------------------------------------------
+
+  case 'subscriptions':
+
+    getByClass("student active", function(element){
+      element.querySelectorAll("a").forEach(function(a){a.href+="/evaluations";})
+    });
+
+    getAll(".student:not(.active)", hide);
+
+    getByClass("users-list photo", flex);
+
+    getByClass("users-list photo", function(element){
+      element.style.flexWrap = "wrap";
+    });
+
+    let done = 0;
+    let avarages = {};
+    getByClass("student active", async function(element){
+
+      var id = element.querySelector("a").href.split("/")[4];
+      var evaluationsURL = "https://oghma.epcc.pt/users/"+id+"/evaluations";
+      var avarage = 0;
+      await fetch(evaluationsURL).then(async html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(await html.text(), 'text/html');
+        avarage = getAvarage(doc);
+
+        element.style.order = "-"+avarage.toFixed(0);
+        avarages[id] = avarage;
+        var averageElement = document.createElement("p");
+        averageElement.textContent = "Média de "+avarage.toFixed(AVARAGE_DECIMAL_PARTS)+" pontos";
+        element.appendChild(averageElement);
+        done++;
+      });
+    });
+
+
     break;
 }
-
-// -------------------------------------------------- Alertas de Exames --------------------------------------------------
-getByText("Inscrições nos Exames", hide);
-getByClass("events announcements", hide);
-getByText("Importante!", hide);
