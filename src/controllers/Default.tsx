bc90e7b3f96642d18e868b30e273220cddd4e330
Leonardo <leo.abreudp@gmail.com>
2023-06-11 13:12:48 +0100

    styling

--- src/controllers/Default.tsx
+++ src/controllers/Default.tsx
@@ -4,7 +4,14 @@ export default class Default extends MainController
 {
   async index()
   {
-    console.log("Welcome to BetterOghma");
+    this.getByClass("well clearfix", element => {
+      const App = () => (
+        <div>
+          <h1>Welcome to BetterOghma</h1>
+        </div>
+      );
+      element.append(VM.m(<App />));
+    });
   }
 
   protected load(): void 
@@ -12,13 +19,5 @@ export default class Default extends MainController
     this.getByText("Inscrições nos Exames", this.hide);
     this.getByClass("events announcements", this.hide);
     this.getByText("Importante!", this.hide);
-    this.getByClass("well clearfix", element => {
-      const App = () => (
-        <div>
-          <h1>Hello, React in Violentmonkey!</h1>
-        </div>
-      );
-      element.append(VM.m(<App />));
-    });
   }
 }
